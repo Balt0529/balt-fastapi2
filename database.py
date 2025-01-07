baa9acd0dbd45ae80e5f84c68a0c2c920993e54d
xkurozaru <kurozaru.210@gmail.com>
2025-01-08 02:59:50 +0900

    style: 💄 format

--- database.py
+++ database.py
@@ -1,14 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
 import os
-from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 # モデル用のBase
 Base = declarative_base()
 
-# .envファイルを読み込む
-load_dotenv()
 
 # 環境変数からデータベースURLを取得
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -18,7 +16,7 @@ if not DATABASE_URL:
 # Engine の作成
 engine = create_engine(
     DATABASE_URL,
-    echo=True  # ログ出力を有効化
+    echo=True,  # ログ出力を有効化
 )
 
 # Sessionの作成
@@ -27,9 +25,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Scoped session (スレッドセーフ)
 ScopedSession = scoped_session(SessionLocal)
 
+
 # データベース初期化
 def init_db():
     Base.metadata.create_all(bind=engine)
+
 
 # データベースセッションを取得する関数
 def get_db():
